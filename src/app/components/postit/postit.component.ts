3ac894ad40f3e6dab2d9a46ff4a272f349396750
Jack Hygate <jack.hygate@myurbanjungle.com>
2025-06-02 23:57:25 +0100

    try to get mobile drag working

--- src/app/components/postit/postit.component.ts
+++ src/app/components/postit/postit.component.ts
@@ -12,25 +12,57 @@ export class PostitComponent {
 
   constructor(private el: ElementRef) {}
 
+  // Mouse Events
   @HostListener('mousedown', ['$event'])
   onMouseDown(event: MouseEvent) {
-    this.isDragging = true;
-    this.offset.x =
-      event.clientX - this.el.nativeElement.getBoundingClientRect().left;
-    this.offset.y =
-      event.clientY - this.el.nativeElement.getBoundingClientRect().top;
+    this.startDrag(event.clientX, event.clientY);
     event.preventDefault();
   }
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    if (!this.isDragging) return;
-    this.el.nativeElement.style.left = `${event.clientX - this.offset.x}px`;
-    this.el.nativeElement.style.top = `${event.clientY - this.offset.y}px`;
+    this.drag(event.clientX, event.clientY);
   }
 
   @HostListener('document:mouseup')
   onMouseUp() {
+    this.endDrag();
+  }
+
+  // Touch Events
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    const touch = event.touches[0];
+    this.startDrag(touch.clientX, touch.clientY);
+    event.preventDefault();
+  }
+
+  @HostListener('document:touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    const touch = event.touches[0];
+    this.drag(touch.clientX, touch.clientY);
+  }
+
+  @HostListener('document:touchend')
+  onTouchEnd() {
+    this.endDrag();
+  }
+
+  // Shared Methods
+  private startDrag(clientX: number, clientY: number) {
+    this.isDragging = true;
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    this.offset.x = clientX - rect.left;
+    this.offset.y = clientY - rect.top;
+  }
+
+  private drag(clientX: number, clientY: number) {
+    if (!this.isDragging) return;
+    this.el.nativeElement.style.left = `${clientX - this.offset.x}px`;
+    this.el.nativeElement.style.top = `${clientY - this.offset.y}px`;
+  }
+
+  private endDrag() {
     this.isDragging = false;
   }
 }
