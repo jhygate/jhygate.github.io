b78316199176ab95cea90a6066d65493070323a3
Jack Hygate <jack.hygate@myurbanjungle.com>
2025-06-04 21:03:23 +0100

    okay it actually works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", // Scan all component templates and TS files
-  ],
-  theme: {
-      backgroundColor: theme => ({
-       ...theme('colors'),
-       'blue-black': '#1e152a',
-       'dark-green': '#4e6766',
-       'light-blue': '#5ab1bb',
-       'light-green': '#a5c882',
-       'yellow': '#f7dd72',
-      })
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                "blue-black": "#1e152a",
+                "dark-green": "#4e6766",
+                "light-blue": "#5ab1bb",
+                "light-green": "#a5c882",
+                "jack-yellow": "#f7dd72",
+            },
+        },
     },
-  plugins: [],
+    plugins: [],
+    important: true,
 };
+
+
