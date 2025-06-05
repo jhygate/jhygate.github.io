f01a4121ef55a77a087532475b67f866ae592352
Jack Hygate <jack.hygate@myurbanjungle.com>
2025-06-05 14:29:31 +0100

    yummy yummy refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,22 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  ViewChild,
-  AfterViewInit,
-} from '@angular/core';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { LeftsideComponent } from './components/leftside/leftside.component';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core'
 
-import { PostitComponent } from './components/postit/postit.component';
+import { ProfileComponent } from './components/profile/profile.component'
+import { ExperienceComponent } from './components/experience/experience.component'
+import { LeftsideComponent } from './components/leftside/leftside.component'
+import { PostitComponent } from './components/postit/postit.component'
+import { MouseGlowComponent } from './components/mouse-glow/mouse-glow.component'
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     ProfileComponent,
     ExperienceComponent,
     LeftsideComponent,
     PostitComponent,
+    MouseGlowComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('maskElement') maskElement!: ElementRef<HTMLDivElement>;
-  @ViewChild('pictureContainer') pictureContainer!: ElementRef<HTMLDivElement>;
-  @ViewChild('toggleButton') toggleButton!: ElementRef<HTMLButtonElement>;
-
-  ngAfterViewInit() {
-    // No need for initial position update since we're using fixed positioning
-  }
-
-  // Remove scroll and resize listeners since we don't need them anymore
-  // The mask will stay fixed in the viewport
-
-  @HostListener('document:pointermove', ['$event'])
-  updateMousePos(pos: PointerEvent) {
-    if (this.maskElement?.nativeElement) {
-      // Calculate position as percentage of viewport
-      const x = (pos.clientX / window.innerWidth) * 100;
-      const y = (pos.clientY / window.innerHeight) * 100;
-
-      // Update CSS custom properties
-      this.maskElement.nativeElement.style.setProperty('--mouse-x', `${x}%`);
-      this.maskElement.nativeElement.style.setProperty('--mouse-y', `${y}%`);
-    }
-  }
-
-  togglePictureContainer() {
-    const container = this.pictureContainer.nativeElement;
-    const button = this.toggleButton.nativeElement;
-
-    container.classList.toggle('collapsed');
-    container.classList.toggle('expanded');
-
-    button.innerHTML = container.classList.contains('expanded')
-      ? '&#x25B2;'
-      : '&#x25BC;';
-  }
-
-  redirectToWebsite(url: string) {
-    window.location.href = url;
-  }
-}
+export class AppComponent {}
