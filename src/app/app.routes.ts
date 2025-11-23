13fb6891cb0608c717b6c9811b4a2c7d3a39b8a4
Jack Hygate <jack.hygate@myurbanjungle.com>
2025-11-23 21:49:53 +0000

    ushing sokaban i think and mj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SokabanComponent } from './sokaban/sokaban.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'sokaban', component: SokabanComponent }
+];
---
+++ src/app/sokaban/sokaban.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sokaban',
+  imports: [],
+  templateUrl: './sokaban.component.html',
+  styleUrl: './sokaban.component.css'
+})
+export class SokabanComponent {
+
+}
